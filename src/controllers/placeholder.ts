6ae4da12bf87bb0a21a4e005383a9e02dce9742e
Demian <git@demiann.dev>
2023-01-27 13:20:29 +0100

    Add cache

--- src/controllers/placeholder.ts
+++ src/controllers/placeholder.ts
@@ -1,6 +1,7 @@
 import { Context } from "worktop";
 
 import { reply } from 'worktop/response';
+import * as cache from '~/helpers/cache'
 
 const generateSvg = (width: number, height: number) => `
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${width}" height="${height}">
@@ -18,10 +19,10 @@ export const index = async (
   const svg = generateSvg(+width, +height)
   // const encodedSvg = encodeSVG(svg)
   // const encodedUrl = `data:image/svg+xml,${encodedSvg}`
-  return new Response(svg, { status: 200, headers: { 'Content-Type': 'image/svg+xml', 'Content-Encoding': 'gzip' } })
+  return new Response(svg, { status: 200, headers: { 'Content-Type': 'image/svg+xml', 'Content-Encoding': 'gzip', ...cache.month } })
 }
 
-const symbols = /[\r\n%#()<>?[\\\]^`{|}]/g;
+// const symbols = /[\r\n%#()<>?[\\\]^`{|}]/g;
 
 // const encodeSVG = (data: string) => {
 //   data = data.replace(/"/g, `'`);
---
+++ src/helpers/cache.ts
@@ -0,0 +1,19 @@
+export const hour = {
+  'Cache-Control': 'max-age=3600, s-maxage=3600',
+};
+
+export const day = {
+  'Cache-Control': 'max-age=86400, s-maxage=86400',
+};
+
+export const week = {
+  'Cache-Control': 'max-age=604800, s-maxage=604800',
+};
+
+export const month = {
+  'Cache-Control': 'max-age=18144000, s-maxage=18144000',
+};
+
+export const year = {
+  'Cache-Control': 'max-age=31536000, s-maxage=31536000',
+};
